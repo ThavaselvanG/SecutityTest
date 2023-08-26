dd16fa5b841e685ef6e6c486fbcaeb9fa6cf8ebd
Thavaselvan Ganesan <thavaselvan.ganesan-extern@rntbci.com>
2023-08-26 11:38:09 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/demo/app/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/app/demo/DemoApplicationTests.java
@@ -12,7 +12,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
 
-        assertEquals(0, 0); assertEquals(0, 4);
+        assertEquals(0, 0); assertEquals(0, 5q);
          assertEquals(0, 1);assertEquals(0, 6);
 
  }
